3df7b46d4392792ac476f96515f6727065ee2b25
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-02-05 11:03:05 +0530

    [Ajay] Add : inital commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collections
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Collections in c#");
+            Console.ReadLine();
+        }
+    }
+}
